e49cd6a2190da178d2d14ac7b789e18cbef40176
Tim Hannes <tim@diztinct.com>
2016-04-15 09:28:09 -0400

    added customer info page, and store info page

--- app.rb
+++ app.rb
@@ -15,6 +15,13 @@ class BigCommerceAPI_Test < Sinatra::Base
       config.api_key = '62edc8aa1cc25be9a2f336b52e408d4a10c1529b'
     end
 
+    # Oauth
+    # Bigcommerce.configure do |config|
+    #   config.store_hash = ENV['BC_STORE_HASH']
+    #   config.client_id = '7w19phg5g9h04fg6kqrixo6vcvqk4e2'
+    #   config.access_token = '4y7aci0hsgi8e3qzt8pg3i1u2u64wqg'
+    # end
+
 
   get '/' do
 
@@ -43,6 +50,24 @@ class BigCommerceAPI_Test < Sinatra::Base
     puts JSON.pretty_generate(@product_images)
 
     erb :images
+
+  end
+
+  get '/customers' do
+
+    @customers = Bigcommerce::Customer.all(page: 1)
+    puts JSON.pretty_generate(@customers)
+
+    erb :customers
+
+  end
+
+  get '/store' do
+
+    @store = Bigcommerce::StoreInfo.info
+    puts JSON.pretty_generate(@store)
+
+    erb :store
 
   end
 
